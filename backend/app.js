cc672e7909ee3e11b659b18c29598078ed2ae00c
idr-yous <idr.yousfi@gmail.com>
2023-12-17 10:43:49 +0100

    connect to a MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 require("dotenv").config();
-
+const mongoose = require("mongoose");
 const app = express();
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.aoqi19i.mongodb.net/?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
