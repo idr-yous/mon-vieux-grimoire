bba6517cd56d961372a5250b3a457be820d9e584
idr-yous <idr.yousfi@gmail.com>
2024-01-22 10:24:23 +0100

    Optimize images using sharp

--- backend/middlewares/multer-config.js
+++ backend/middlewares/multer-config.js
@@ -1,4 +1,7 @@
 const multer = require("multer");
+const sharp = require("sharp");
+sharp.cache(false);
+const fs = require("fs");
 
 const MIME_TYPE = {
   "image/jpg": "jpg",
@@ -7,18 +10,42 @@ const MIME_TYPE = {
   "image/webp": "webp",
 };
 
-const storage = multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, "./images");
-  },
-  filename: (req, file, callback) => {
-    const filename = file.originalname.split(" ").join("_");
-    const filenameArray = filename.split(".");
-    filenameArray.pop();
-    const filenameWithoutExtention = filenameArray.join(".");
-    const extension = MIME_TYPE[file.mimetype];
-    callback(null, filenameWithoutExtention + Date.now() + "." + extension);
-  },
-});
+const storage = multer.memoryStorage();
+const upload = multer({ storage }).single("image");
 
-module.exports = multer({ storage }).single("image");
+module.exports = (req, res, next) => {
+  upload(req, res, async (err) => {
+    if (err) {
+      return res.status(400).json({ error: err.message });
+    }
+
+    // Check if there is an image file to process
+    if (req.file) {
+      try {
+        // Customize image filename
+        const filename = req.file.originalname.split(" ").join("_");
+        const filenameArray = filename.split(".");
+        filenameArray.pop();
+        const filenameWithoutExtension = filenameArray.join(".");
+        const ref = `${filenameWithoutExtension + Date.now()}.webp`;
+
+        // Resize and convert to Webp using sharp
+        const buffer = await sharp(req.file.buffer)
+          .resize({ width: 206, height: 260 })
+          .toFormat("webp", { quality: 85 })
+          .toBuffer();
+
+        // Save the WebP version
+        fs.writeFileSync(`./images/${ref}`, buffer);
+
+        // Update req.file.filename
+        // the file name is used in book routes to get image url
+        req.file.filename = ref;
+      } catch (error) {
+        return res.status(500).json({ error: "Error processing image" });
+      }
+    }
+
+    next();
+  });
+};
